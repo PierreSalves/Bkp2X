a72e82ee879313f3d27d10821dffdd139ade3081
PierreSalves <pierre040797@live.com>
2023-08-28 19:51:35 -0300

    .

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -6,20 +6,21 @@
  * This file is application-wide controller file. You can put all
  * application-wide controller-related methods here.
  *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
  * @package       app.Controller
  * @since         CakePHP(tm) v 0.2.9
- * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('Controller', 'Controller');
 
 /**
@@ -29,31 +30,44 @@ App::uses('Controller', 'Controller');
  * will inherit them.
  *
  * @package		app.Controller
- * @link		https://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller
 {
-	public $components = array(
+
+	var $components = array(
 		'DebugKit.Toolbar',
+		'Cookie',
+		'Auth',
 		'Session',
-		'Auth' => array(
-			'userModel' => 'Usuario', // Especifique o nome do modelo de usuário personalizado
-			'loginAction' => array(
-				'controller' => 'Usuario',
-				'action' => 'login'
-			),
-			'Form' => array(
-				'fields' => array(
-					'id' => 'usercodigo'
-				)
-			),
-			'loginRedirect' => array('controller' => 'Backups', 'action' => 'index'),
-			'logoutRedirect' => array('controller' => 'Usuario', 'action' => 'login')
-		),
 	);
 
-	// public function beforeFilter()
-	// {
-	// 	$this->Auth->allow('index', 'view');
-	// }
+
+	public function beforeFilter()
+	{
+		parent::beforeFilter();
+
+
+		// $this->Auth->loginAction = array('controller' => 'Professor', 'action' => 'login' ); //o usuário é redirecionado para esta ação caso não esteja autenticado
+		// $this->Auth->logoutRedirect = array('controller' => 'Professor', 'action' => 'login'); //redireciona após o logout
+		// $this->Auth->loginRedirect = array('controller' => 'Professor', 'action' => 'index'); //redireciona após login
+
+		// $this->Auth->loginError = __('Usuário e/ou senha inválido(s).', true);
+		// $this->Auth->authError = __('Você precisa fazer login para acessar esta página.', true);
+
+		$this->Auth->loginAction = array('controller' => 'Usuario', 'action' => 'login');
+		$this->Auth->loginRedirect = array('controller' => 'Backups', 'action' => 'index');
+		$this->Auth->logoutRedirect = array('controller' => 'Usuario', 'action' => 'login');
+		$this->Auth->authenticate = array(
+			'Form' => array(
+				'userModel' => 'Usuario',
+				'fields' => array(
+					'username' => 'userlogin',
+					'password' => 'userpassword'
+				)
+			)
+		);
+
+		date_default_timezone_set('America/Sao_Paulo');
+	}
 }
